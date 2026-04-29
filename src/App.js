2f026b69bb3f7bc5b0d31dfc565b5627ffcd9080
João Pedro Miguel <joao.p.silva444@aluno.senai.br>
2026-04-29 08:24:16 -0300

    main

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import Header from './componentes/Header';
+import styled from 'styled-components';
+import Pesquisa from './componentes/Pesquisa';
+import UltimosLancamentos from './componentes/UltimosLancamentos';
+
+// Container que guarda estilos (styled components)
+const AppContainer = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background-image: linear-gradient(90deg, #002F52 35%, #326589);
+`
+
+function App() {
+  return (
+    <AppContainer>
+      <Header />
+      <Pesquisa />
+      <UltimosLancamentos/>
+    </AppContainer>
+  );
+}
+
+export default App;
---
+++ src/componentes/Logo/index.js
@@ -0,0 +1,25 @@
+import logo from '../../imagens/logo.svg'
+import styled from 'styled-components'
+
+const LogoDiv = styled.div`
+
+  display: flex;
+  font-size: 30px;
+
+`
+const LogoImg = styled.img`
+
+margin-right: 10px;
+
+`
+
+function Logo() {
+    return (
+        <LogoDiv>
+            <LogoImg src={logo} alt='logo'></LogoImg>
+            <strong><p>SENAI Books</p></strong>
+        </LogoDiv>
+    )
+}
+
+export default Logo
