9b73c26f1cea49556467ece1ab21d6ebbbe9a1bd
João Pedro Miguel <joao.p.silva444@aluno.senai.br>
2026-06-03 08:48:02 -0300

    src componentes

--- src/componentes/UltimosLancamentos/index.js
+++ src/componentes/UltimosLancamentos/index.js
@@ -1,21 +1,15 @@
-import { livros } from "./dadosUltimosLancamentos"
-import styled from "styled-components"
+import { livros } from "./dadosUltimosLancamentos";
+import styled from "styled-components";
+import { Titulo } from "../Titulo";
+import CardRecomenda from "../CardRecomenda";
+import imagemLivro from '../../imagens/livro2.png'
+
 
 const UltimosLancamentosContainer = styled.section`
 background-color: #EBECEE;
 padding-bottom: 20px;
 display: flex;
 flex-direction: column;
-`
-
-const Titulo = styled.h2`
-width: 100%;
-padding: 30px 0;
-background-color: #FFF;
-color: #EB9B00;
-font-size: 36px;
-text-align: center;
-margin:0;
 `
 
 const NovosLivrosContainer = styled.div`
@@ -29,12 +23,18 @@ cursor: pointer;
 function UltimosLancamentos() {
     return (
         <UltimosLancamentosContainer>
-            <Titulo>ULTIMOS LANÇAMENTOS</Titulo>
+            <Titulo cor={"#000"} tamanhoFonte={"36px"}>ULTIMOS LANÇAMENTOS</Titulo>
             <NovosLivrosContainer>
             {livros.map(livro => (
                 <img src={livro.src} />
             ))}
             </NovosLivrosContainer>
+            <CardRecomenda
+               titulo="Talvez você se iteresse por..."
+               subtitulo="Angular 11"
+               descricao="Construindo uma aplicação com a plataforma Google."
+               img={imagemLivro}
+            />
         </UltimosLancamentosContainer>
     )
 }
